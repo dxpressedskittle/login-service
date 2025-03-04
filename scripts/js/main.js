b27b17eb12fece7c1e298acedf7df7b94d6ce995
dxpressedskittle <hinuto026@gmail.com>
2025-03-03 19:06:09 -0500

    s

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -1,3 +1,43 @@
+let buttons = [
+    { 
+        id: 'button1', 
+        text: 'Button 1', 
+        onClick: function() { 
+            alert('Button 1 clicked'); 
+        } 
+    },
+    { 
+        id: 'button2', 
+        text: 'Button 2', 
+        onClick: function() { 
+            alert('Button 2 clicked'); 
+        } 
+    },
+    { 
+        id: 'button3', 
+        text: 'Button 3', 
+        onClick: function() { 
+            alert('Button 3 clicked'); 
+        }
+    }
+];
+
+let initialized = false;
+
 function initializeUI() {
-    
-};
+    if (initialized) {
+        return;
+    };
+
+    initialized = true;
+
+    buttons.forEach(button => {
+        let btnElement = document.createElement('button');
+        btnElement.id = button.id;
+        btnElement.innerText = button.text;
+        btnElement.addEventListener('click', button.onClick);
+        document.body.appendChild(btnElement);
+    });
+}
+
+initializeUI();
